ada19a1ad1232dcbea68648b47de237406c324fc
indieka900 <indiekaj@gmail.com>
2023-06-15 16:48:41 +0300

    Job Search

--- lib/services/helpers/jobs_helper.dart
+++ lib/services/helpers/jobs_helper.dart
@@ -47,19 +47,27 @@ class JobsHelper {
       throw Exception("Failed to get a job");
     }
   }
+
   static Future<List<JobsResponse>> searchJob(String search) async {
     //final SharedPreferences prefs = await SharedPreferences.getInstance();
     Map<String, String> requestHeaders = {
       'Content-Type': 'application/json',
     };
+    String url = 'https://${Config.apiUrl}${Config.search}?query=$search';
+    int index = url.indexOf('@20');
+    if (index != -1) {
+      url = url.substring(0, index);
+    }
 
-    var url = Uri.https(Config.apiUrl, '${Config.search}?query=$search');
+    var modified = Uri.parse(url);
+    //print(url);
     //await prefs.setBool('loading', true);
     var response = await client.get(
-      url,
+      modified,
       headers: requestHeaders,
     );
     //await prefs.setBool('loading', false);
+    //print(response.statusCode);
     if (response.statusCode == 200) {
       var jobList = jobsResponseFromJson(response.body);
       return jobList;
